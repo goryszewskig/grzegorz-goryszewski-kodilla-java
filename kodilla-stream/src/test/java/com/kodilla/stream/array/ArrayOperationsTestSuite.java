9a9dd9d9dff2622ccac6f35775464c82ab40a0e8
Grzegorz Goryszewski <greg.szewski@gmail.com>
2017-11-18 11:39:51 +0100

    Do 7.5

---
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -0,0 +1,42 @@
+package com.kodilla.stream.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+import java.util.Random;
+
+import static com.kodilla.stream.array.ArrayOperations.getAverage;
+
+public class ArrayOperationsTestSuite {
+    private int[] getTestArray(int size, int range) {
+        int[] numbers = new int[size];
+        Random generator = new Random();
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = generator.nextInt(range);
+        }
+        return numbers;
+    }
+
+    private double getExpectedAverage(int[] numbers) {
+        double n = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            n += numbers[i];
+        }
+        return n / numbers.length;
+    }
+
+    @Test
+    public void testGetAverage() {
+        //Given
+        int[] numbers = getTestArray(92,11);
+
+        //When
+        double expectedAverage = getExpectedAverage(numbers);
+        double testedAverage = getAverage(numbers);
+
+
+        //Then
+        Assert.assertEquals(expectedAverage, testedAverage, 0);
+    }
+
+}
