4a74b2b32f486db217b2d14f8dc9356ffbd4f4ca
Grzegorz Goryszewski <greg.szewski@gmail.com>
2017-11-28 21:46:59 +0100

    9.2

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/SalesStatusService.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/SalesStatusService.java
@@ -1,10 +1,13 @@
 package com.kodilla.good.patterns.challenges;
 
+import com.sun.istack.internal.NotNull;
+
 import java.time.LocalDate;
 
 public class SalesStatusService implements OrderService{
 
-    public boolean status(int orderNumber, LocalDate deliveryDate, double orderPrice){
+    public boolean status(int orderNumber, @NotNull LocalDate deliveryDate, double orderPrice){
+
         LocalDate now = LocalDate.now();
         if(deliveryDate.isAfter(now)){
             System.out.println("Delivery at " + deliveryDate + " Price" + orderPrice );
