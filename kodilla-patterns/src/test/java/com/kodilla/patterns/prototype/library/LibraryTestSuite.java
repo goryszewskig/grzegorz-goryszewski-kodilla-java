68134c2f9d5b0901072b2b865dc42269cbdc7418
Grzegorz Goryszewski <greg.szewski@gmail.com>
2017-12-14 20:05:28 +0100

    11.2 - prototype

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
@@ -0,0 +1,55 @@
+package com.kodilla.patterns.prototype.library;
+
+import com.kodila.patterns.prototype.library.Book;
+import com.kodila.patterns.prototype.library.Library;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.stream.IntStream;
+
+public class LibraryTestSuite {
+    @Test
+    public void testGetBooks() {
+        //Given
+        Library library = new Library("Library 1");
+        IntStream.iterate(1, n -> n + 1)
+                .limit(9)
+                .forEach(n -> library.getBooks()
+                        .add(new Book("Book" + n, "Author " +
+                                n, LocalDate.of(2000 + n, Month.JANUARY, 1 + n))));
+
+        //shallow copy of object library
+        Library clonedLibrary = null;
+        try {
+            clonedLibrary = library.shallowCopy();
+            clonedLibrary.setName("Library 2");
+        } catch (CloneNotSupportedException e) {
+            System.out.println(e);
+        }
+
+        //deep copy of object library
+        Library deepClonedLibrary = null;
+        try {
+            deepClonedLibrary = library.deepCopy();
+            deepClonedLibrary.setName("Library 3");
+        } catch (CloneNotSupportedException e) {
+            System.out.println(e);
+        }
+
+        //When
+        library.getBooks().add(new Book("newBook", "Artur Connan", LocalDate.of(2000, Month.APRIL, 10)));
+
+        //Then
+        System.out.println(library.getBooks());
+        System.out.println(clonedLibrary.getBooks());
+        System.out.println(deepClonedLibrary.getBooks());
+        Assert.assertEquals(10, library.getBooks().size());
+        Assert.assertEquals(10, clonedLibrary.getBooks().size());
+        Assert.assertEquals(9, deepClonedLibrary.getBooks().size());
+        Assert.assertEquals(library.getBooks(), clonedLibrary.getBooks());
+        Assert.assertNotEquals(library.getBooks(), deepClonedLibrary.getBooks());
+    }
+
+}
